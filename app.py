33db1568b1008c1ffe2283fee54aa3b56ea4644d
ischaojie <zhuzhezhe95@gmail.com>
2022-02-08 22:42:47 +0800

    Feat: templates setup and run app

---
+++ app.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import redis
+import uvicorn
+from starlette.applications import Starlette
+
+from mocon.main import Mocon
+from mocon.models import BaseModel
+
+app = Starlette(debug=True)
+db = redis.Redis(host='localhost', port=6379, db=0)
+mocon = Mocon(app)
+
+
+class FavoriteMovieModel(BaseModel):
+    class Meta:
+        source = db
+        can_view = True
+
+    title: str
+    description: str
+    director: str
+    pub_year: int
+
+
+mocon.register(FavoriteMovieModel)
+
+if __name__ == '__main__':
+    uvicorn.run("app:app", host="127.0.0.1", port=8000)
---
+++ tests/test_models.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from mocon.models import BaseModel
+
+def test_base_model_():
+    pass
