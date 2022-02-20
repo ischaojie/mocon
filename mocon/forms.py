4f14f28652469614bd7983ff9552e9dddc706b66
ischaojie <zhuzhezhe95@gmail.com>
2022-02-20 11:08:51 +0800

    feat: pydantic field convert to wtforms field

--- mocon/forms.py
+++ mocon/forms.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from typing import Any, Callable, Dict, Sequence, Union, no_type_check
+
 from pydantic import BaseModel
-from wtforms import Form, StringField
-from typing import Any, Callable, Dict, Sequence, no_type_check
+from pydantic.fields import ModelField
+from wtforms import Form, StringField, validators, Field
 
 
 @no_type_check
@@ -19,20 +21,42 @@ class BaseConverter:
         # conv_mapping for each BaseModel type_ mapping to a converter
         # for example: type `str` mapping to wtforms StringField
         conv_mapping = {}
-        for name in dir(self):
-            obj = getattr(self, name)
+        for method in dir(self):
+            obj = getattr(self, method)
             if hasattr(obj, "_converter_for"):
                 for arg in obj._converter_for:
                     conv_mapping[arg] = obj
         self.converters = conv_mapping
-    
-    def switch_converter():
-        pass
+
+    def convert(self, field: ModelField):
+        # https://wtforms.readthedocs.io/en/3.0.x/fields/#wtforms.fields.Field.__init__
+
+        field_info = field.field_info
+
+        field_args = {
+            "validators": [],
+            "filters": [],
+            "description": field_info.description,
+            "default": field_info.default,
+            "name": field_info.title,
+        }
+        # add validators
+        if field.required:
+            field_args["validators"].append(validators.InputRequired())
+        else:
+            field_args["validators"].append(validators.Optional())
+
+        # TODO: validator length (min=- 1, max=- 1, message=None)
+
+        converter = self.converters.get(field.type_.__name__)
+        if not converter:
+            pass
+        return converter(**field_args)
 
 
 class Converter(BaseConverter):
     @convert("str")
-    def conv_str(self, field_args: Dict, **kwargs: Any):
+    def conv_str(self, field_args: Dict, **kwargs: Any) -> Field:
         """convert str type to StringField"""
         return StringField(**field_args)
 
@@ -51,7 +75,7 @@ def model_to_form(
             continue
         if exclude and name in exclude:
             continue
-        form_field = conveter.convert(model, model_field.type_)
+        form_field = conveter.convert(model, model_field)
         if form_field:
             form_fields[name] = form_field
 
